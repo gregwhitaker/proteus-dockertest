601969fa4644243c12f83107d75e155bf4e1e6a1
gregwhitaker <gwhitake@gmail.com>
2018-07-21 14:01:47 -0700

    Doing work

--- dockertest-client/src/main/java/com/netifi/proteus/dockertest/client/Main.java
+++ dockertest-client/src/main/java/com/netifi/proteus/dockertest/client/Main.java
@@ -15,9 +15,14 @@
  */
 package com.netifi.proteus.dockertest.client;
 
+import com.netifi.proteus.dockertest.service.DockerTestRequest;
+import com.netifi.proteus.dockertest.service.DockerTestServiceClient;
 import com.netifi.proteus.springboot.EnableProteus;
+import io.netifi.proteus.annotations.ProteusClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableProteus(group = "dockertest.clients")
@@ -25,5 +30,23 @@ public class Main {
 
     public static void main(String... args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    /**
+     * Subscribes to the DockerTestService and prints responses.
+     */
+    @Component
+    public class Runner implements CommandLineRunner {
+
+        @ProteusClient(group = "dockertest.services")
+        private DockerTestServiceClient client;
+
+        @Override
+        public void run(String... args) throws Exception {
+            client.runTest(DockerTestRequest.newBuilder().build())
+                    .subscribe(response -> System.out.println(response.getNumber()));
+
+            Thread.currentThread().join();
+        }
     }
 }
